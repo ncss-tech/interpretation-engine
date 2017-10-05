c1e7add958acc085e244bb5d9c8aa2a19198d51d
Dylan Beaudette <dylan.beaudette@gmail.com>
2017-10-05 15:58:02 -0700

    adding experimental www report interface to properties

---
+++ get-propert-by-name-coiid.R
@@ -0,0 +1,11 @@
+library(sharpshootR)
+
+url <- 'https://nasis.sc.egov.usda.gov/NasisReportsWebSite/limsreport.aspx?report_name=WEB-PROPERY-COMPONENT_property'
+
+# prop_name: full property name
+# cokey: component record ID, can be a comma-delim list
+args <- list(prop_name='TAXONOMIC SUBORDER', cokey='1842387')
+
+parseWebReport(url, args, index=1)
+
+
