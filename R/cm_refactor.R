1260800127d1268e1fb3ebb77fc7681a4843d0c3
Andrew Gene Brown <brown.andrewg@gmail.com>
2024-05-21 07:05:42 -0700

    move some less essential packages to suggests

--- R/cm_refactor.R
+++ R/cm_refactor.R
@@ -2,30 +2,31 @@
 ### all it does is refactor two inputs and do the CM
 ### joe brehm 3-23-21
 
-# require(caret)
-
-#' Helper function to make confusion matricies easier
+#' Helper function to make confusion matrices easier
+#' 
 #' Refactor two inputs and make confusion matrix with caret package
-#' @param predvar predictor var
-#' @param refvar reference var
+#' 
+#' @param predvar Predictor variable
+#' @param refvar Reference variable
 #' @author Joseph Brehm
 #'
-#' @return a caret confusion matrix
+#' @return A caret confusion matrix
 #' @export
-#' @importFrom caret confusionMatrix
-#' @importFrom forcats fct_explicit_na
 cm_refactor <- function(predvar, refvar){
   # refactor both inputs to have the same levels as one another
   
-  refvar <- fct_explicit_na(as.character(refvar))
-  predvar <- fct_explicit_na(as.character(predvar))
+  stopifnot(requireNamespace("caret"))
+  stopifnot(requireNamespace("forcats"))
+  
+  refvar <- forcats::fct_explicit_na(as.character(refvar))
+  predvar <- forcats::fct_explicit_na(as.character(predvar))
   
   predvar <- factor(x = predvar, 
                     levels = levels(refvar))
   refvar <- factor(x = refvar,
                    levels = levels(predvar))
   
-  cm <- confusionMatrix(reference = refvar,
+  cm <- caret::confusionMatrix(reference = refvar,
                         data = predvar)
   return(cm)
 }
