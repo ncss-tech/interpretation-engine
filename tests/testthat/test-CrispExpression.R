35e6fe712f7665a724887238ac6a073f1494131b
Andrew Gene Brown <andrew.g.brown@usda.gov>
2024-07-16 10:27:14 -0700

    Add CrispExpression tests

---
+++ tests/testthat/test-CrispExpression.R
@@ -0,0 +1,34 @@
+test_that("crisp expression parsing works", {
+  # check that case is ignored for logical operators in crispexpression
+  # debug(initRuleset)
+  # debug(.crispFunctionGenerator)
+  # 
+  
+ # test various rules that have specific handling in the parser
+  
+ expect_silent(initRuleset(rulename = "FOR - Potential Erosion Hazard, Road/Trail, Spring Thaw (AK)"))
+  
+ expect_silent(initRuleset(rulename = "AGR - Spring Wheat Yield (MT)"))
+  
+ expect_silent(initRuleset(rulename = "ENG - Daily Cover for Landfill (AK)"))
+  
+ expect_silent(initRuleset(rulename = "CLR-cropland limitation for wheat"))
+  
+ expect_silent(initRuleset(rulename = "WLF - Desert Tortoise (CA)"))
+  
+ expect_silent(initRuleset(rulename = "ENG - OSHA Soil Types (TX)"))
+  
+ expect_silent(initRuleset(rulename = "Ground Penetrating Radar Penetration"))
+  
+ expect_silent(initRuleset(rulename = "WMS - Subsurface Drains - Installation (VT)"))
+  
+ expect_silent(initRuleset(rulename = "Valley Fever"))
+  
+ expect_silent(initRuleset(rulename = "LCC - NIRR Land Capability Class"))
+  
+ expect_silent(initRuleset(rulename = "FOR - Windthrow Hazard (WA)"))
+ 
+ expect_silent(initRuleset(rulename = "LCC-irr"))
+ 
+ expect_silent(initRuleset(rulename = "FOR - Conservation Tree/Shrub Groups (MT)"))
+})
